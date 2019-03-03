76983e761af6bb2144f86f97c31027e5025855d8
Shanthosh Krishnakumar <shanthosh@workhere.co.nz>
2019-03-04 11:34:17 +1300

    sample code to show If there is no jsx. Later commented

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
         <h1>Hi, This is a React app!</h1>
       </div>
     );
+
+    // return React.createElement(
+    //   'div',
+    //   { className: 'App' },
+    //   React.createElement('h1', 'null', 'Hi, This is a React app!!!')
+    // );
   }
 }
 

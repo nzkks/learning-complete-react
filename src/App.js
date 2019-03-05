336c752eb6b3152464735fec0c8c096531625093
Shanthosh Krishnakumar <shanthosh@workhere.co.nz>
2019-03-06 11:58:02 +1300

    Create flexible lists

--- src/App.js
+++ src/App.js
@@ -14,15 +14,19 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangeHandler = event => {
-    this.setState({
-      persons: [
-        { name: 'Shan', age: 32 },
-        { name: event.target.value, age: 25 },
-        { name: 'Rish', age: 14 },
-        { name: 'Darsh', age: 9 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   deletePersonHandler = personIndex => {
@@ -59,30 +63,10 @@ class App extends Component {
                 name={person.name}
                 age={person.age}
                 key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
-
-          {/* <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-          />
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-            click={this.switchNameHandler.bind(this, 'Shantho')} // recommended
-            changed={this.nameChangeHandler}
-          >
-            My Hobbies: Drawing
-          </Person>
-          <Person
-            name={this.state.persons[3].name}
-            age={this.state.persons[3].age}
-          /> */}
         </div>
       );
     }

9a81e9c17aaa0037195c9359da7d1d3e643da0eb
nzkks <kkshanthosh@gmail.com>
2019-03-09 21:09:34 +1300

    render inner adjecent JSX elements as an array

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import styles from './Person.module.css';
+// import styles from './Person.module.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
 
-    return (
-      <div className={styles.person}>
-        <p onClick={this.props.click}>
-          I'm {this.props.name} and I am {this.props.age} years old!
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.changed}
-          value={this.props.name}
-        />
-      </div>
-    );
+    //* We can also remove the root level JSX element and render inner adjecent JSX elements as an array
+    return [
+      // <div className={styles.person}>
+      <p key="item1" onClick={this.props.click}>
+        I'm {this.props.name} and I am {this.props.age} years old!
+      </p>,
+      <p key="item2">{this.props.children}</p>,
+      <input
+        key="item3"
+        type="text"
+        onChange={this.props.changed}
+        value={this.props.name}
+      />
+      // </div>
+    ];
   }
 }
 

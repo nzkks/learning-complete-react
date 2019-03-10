c6f434d399b507fffefdab30b6eebd6d2b1ff4b6
nzkks <kkshanthosh@gmail.com>
2019-03-10 21:27:14 +1300

    Use contextType in class based components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -11,6 +11,8 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     //* regular client based JavaScript way of targeting an element. This will always select the first element.
     // document.querySelector('input').focus();
@@ -19,6 +21,8 @@ class Person extends Component {
     // this.inputElement.focus();
 
     this.inputElementRef.current.focus();
+
+    console.log(this.context.authenticated);
   }
 
   render() {
@@ -31,11 +35,11 @@ class Person extends Component {
       //* use below Aux - root level wrapper to render adjecent JSX elements without any html wrapper element like 'div'.
       // <Aux>
       <Fragment>
-        <AuthContext.Consumer>
-          {context =>
-            context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please log in</p>
+        )}
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>

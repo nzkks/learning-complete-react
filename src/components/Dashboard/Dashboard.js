b183915327f9aace01463b4f87b1fc1cf9a61cd1
nzkks <kkshanthosh@gmail.com>
2019-03-10 21:31:19 +1300

    Make use of useContext() in functional components

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Dashboard.module.css';
 import AuthContext from '../../context/auth-context';
 
 const dashboard = props => {
   const toggleBtnRef = useRef(null);
+
+  const authContext = useContext(AuthContext);
+  console.log(authContext.authenticated);
 
   //* useEffect - componentDidMount and ComponentDidUpdate combined in one effect
   useEffect(() => {
@@ -56,9 +59,8 @@ const dashboard = props => {
       <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
-      <AuthContext.Consumer>
-        {context => <button onClick={context.login}>Log in</button>}
-      </AuthContext.Consumer>
+
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 };
